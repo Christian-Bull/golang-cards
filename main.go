cbc71fe8cfe6990278190d4257db7a774bd7558c
Christian Bull <cbull@ChristianPC.fios-router.home>
2019-12-16 22:49:01 -0500

    added more testing

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
+	// get new deck and save it to file
 	deck := newDeck()
 	deck.saveToFile("./assets/my_deck")
 
+	// get the saved file
 	cards := newDeckFromFile("./assets/my_deck")
 
 	cards.shuffle()
