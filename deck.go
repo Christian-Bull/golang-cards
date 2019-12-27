f7d8905ffa8dcfefd640671cc96a4e1b7a134aa5
Christian <cbull.projects@gmail.com>
2019-12-27 14:30:11 -0500

    added x dealing and x shuffling funcs

--- deck.go
+++ deck.go
@@ -34,14 +34,35 @@ func deal(d tDeck, handSize int) (tDeck, tDeck) {
 	return hand, d
 }
 
-func (d tDeck) shuffle() {
+func dealX(d tDeck, handSize int, numHands int) []tDeck {
+	var hands []tDeck
+
+	for i := 0; i < numHands; i++ {
+		hand, rHand := deal(d, handSize)
+		hands = append(hands, hand)
+		d = rHand
+	}
+	return hands
+}
+
+func newR() *rand.Rand {
 	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
+	return rand.New(source)
+}
+
+func (d tDeck) newPositions() {
+	r := newR()
 
 	for i := range d.cards {
 		newPosition := r.Intn(len(d.cards) - 1)
 
 		d.cards[i], d.cards[newPosition] = d.cards[newPosition], d.cards[i]
+	}
+}
+
+func (d tDeck) shuffle(j int) {
+	for i := 0; i < j; i++ {
+		d.newPositions()
 	}
 }
 
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	// get new deck and save it to file
 	sDeck := newDeckStruct()
 
 	// test shuffle func
-	//sDeck.shuffle()
-	//fmt.Println(sDec
-
+	sDeck.shuffle(5)
 	sDeck.print()
+
+	x := dealX(sDeck, 5, 3)
+	fmt.Println(x)
 }
